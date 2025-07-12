be9df106f5afc4fe0d823f8f3348f21b1fcb5411
Sompalkar <palkarsom1@gmail.com>
2025-07-13 00:20:46 +0530

    userside

---
+++ user-side/app/page.tsx
@@ -0,0 +1,11 @@
+import VideoFlowLanding from "@/components/landing-page";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <> 
+    <VideoFlowLanding/>
+    
+    </>
+  );
+}
