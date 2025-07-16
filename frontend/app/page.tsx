13971d1acdae13460226506c1564f392864f27cc
Sompalkar <palkarsom1@gmail.com>
2025-07-16 23:48:25 +0530

    small changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import VideoFlowLanding from "@/components/landing-page";
+  import VideoFlowLanding from "@/components/landing-page";
 import Image from "next/image";
 
 export default function Home() {
